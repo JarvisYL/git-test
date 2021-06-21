403c657bb971fe1bb17253f5815347d6d635c59b
‘JarvisYL’ <bingbdc@163.com>
2021-06-21 15:23:41 +0800

    hot-fix commit1

--- src/main/java/org/example/git/gitTest.java
+++ src/main/java/org/example/git/gitTest.java
@@ -2,8 +2,10 @@ package org.example.git;
 
 public class gitTest {
     public static void main(String[] args) {
-        System.out.println("hello git!");
-        System.out.println("hello second");
-        System.out.println("hello third");
+        System.out.println("hello git1");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+
     }
 }
