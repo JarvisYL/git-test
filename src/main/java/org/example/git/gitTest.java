bde6afee440890838e966410c1b10e02fd10bc56
‘JarvisYL’ <bingbdc@163.com>
2021-06-21 14:30:09 +0800

    second commit

--- src/main/java/org/example/git/gitTest.java
+++ src/main/java/org/example/git/gitTest.java
@@ -3,5 +3,6 @@ package org.example.git;
 public class gitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello second");
     }
 }
